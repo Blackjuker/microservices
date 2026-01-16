6e293975f11dce8e5fcfc28838cb5865a0d8b179
NGABGNA Arnaud <ANGA@cyclife.com>
2026-01-16 10:24:00 +0100

    ajout du repository

--- Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Play.Catalog.Service.Controllers
@@ -8,11 +9,72 @@ namespace Play.Catalog.Service.Controllers
     [Route("items")]
     public class ItemsController : ControllerBase
     {
-        private readonly List<ItemDto> items = new()
+        private static readonly List<ItemDto> items = new()
         {
             new ItemDto(Guid.NewGuid(), "Item1", "The first item", 9.99m, DateTimeOffset.UtcNow),
             new ItemDto(Guid.NewGuid(), "Item2", "The second item", 19.99m, DateTimeOffset.UtcNow),
             new ItemDto(Guid.NewGuid(), "Item3", "The third item", 29.99m, DateTimeOffset.UtcNow)
         };
+
+        [HttpGet]
+        public IEnumerable<ItemDto> GetItems()
+        {
+            return items;
+        }
+
+        // Get /items/
+        [HttpGet("{id}")]
+        public ActionResult<ItemDto> GetById(Guid id)
+        {
+            var item = items.Where(item => item.Id == id).SingleOrDefault();
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+
+        [HttpPost]
+        public ActionResult CreateItem(CreateItemDto createItemDto)
+        {
+            var item = new ItemDto(Guid.NewGuid(), createItemDto.Name, createItemDto.Description, createItemDto.Price, DateTimeOffset.UtcNow);
+            items.Add(item);
+            return CreatedAtAction(nameof(GetById), new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateItem(Guid id, UpdateItemDto updateItemDto)
+        {
+            var existingItem = items.Where(item => item.Id == id).SingleOrDefault();
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
+
+            var updatedItem = existingItem with
+            {
+                Name = updateItemDto.Name,
+                Description = updateItemDto.Description,
+                Price = updateItemDto.Price
+            };
+
+            var index = items.FindIndex(item => item.Id == id);
+            items[index] = updatedItem;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteItem(Guid id)
+        {
+            var existingItem = items.Where(item => item.Id == id).SingleOrDefault();
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
+
+            items.Remove(existingItem);
+            return NoContent();
+        }
     }
 }
