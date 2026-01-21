83e96cf4f00e3c56ff00954369a1216dd3a2f3af
NGABGNA Arnaud_adm <anga_adm@SOCODOM.FR>
2026-01-21 17:43:02 +0100

    update le temps de manière random pour que tout le monde n'a pas le meme temps d'attente

--- Play.Inventory/src/Play.Inventory.Service/Startup.cs
+++ Play.Inventory/src/Play.Inventory.Service/Startup.cs
@@ -29,12 +29,16 @@ namespace Play.Inventory.Service
 
             services.AddMongo()
                 .AddMongoRepository<Entities.InventoryItem>("inventoryitems");
+            
+            Random jitterer = new Random();
 
                 services.AddHttpClient<CatalogClient>(client =>
                 {
                     client.BaseAddress = new Uri("https://localhost:5001");
                 })
-                .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(5, 
+                retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+                + TimeSpan.FromMilliseconds(jitterer.Next(0,1000)),
                  onRetry: (outcome, timespan, retryAttempt, context) => 
                  {
                      //log details of the retry
