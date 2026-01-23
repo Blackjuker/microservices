5197c03720f62af8ff513e6dbc2d5e7e81503891
NGABGNA Arnaud_adm <anga_adm@SOCODOM.FR>
2026-01-23 21:27:16 +0100

    create contracts

---
+++ Play.Catalog/src/Play.Catalog.Contracts/Contracts.cs
@@ -0,0 +1,7 @@
+using System;
+namespace Play.Catalog.Contracts
+{
+    public record CatalogItemCreated(Guid ItemId, string Name, string Description);
+    public record CatalogItemUpdate(Guid ItemId, string Name, string Description);
+    public record CatalogItemDeleted(Guid ItemId);
+}
